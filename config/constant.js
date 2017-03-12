18dde355cfa24f820ab464ef91ba9400ba15c922
thusitha@crowderia.com <thusitha@crowderia.com>
2017-03-12 18:29:43 +0530

    ground set

---
+++ config/constant.js
@@ -0,0 +1,30 @@
+export const Application = {
+
+    game: {
+        pi: {
+            pi = Math.PI
+        },
+
+        player: {
+            width: 20,
+            height: 100
+        },
+
+        ai: {
+            width: 20,
+            height: 100
+        },
+
+        ball: {
+            side: 20
+        }
+
+    },
+
+    ui: {
+        canvas: {
+            WIDTH = 700,
+            HEIGHT = 600
+        }
+    }
+}
